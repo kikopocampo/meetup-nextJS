a42d589dbfde63611718198e2bc70018ce4ad20e
Francisco <kikopocampo@gmail.com>
2023-01-01 16:01:07 -0700

    fix fallback

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2fd636eeaebdef09"; }
+/******/ 		__webpack_require__.h = function() { return "af36837d885285ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.a93931b89bb51859.hot-update.js
+++ .next/static/webpack/webpack.a93931b89bb51859.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2fd636eeaebdef09"; }
+/******/ 	__webpack_require__.h = function() { return "f580b27def4674be"; }
 /******/ }();
 /******/ 
 /******/ }
